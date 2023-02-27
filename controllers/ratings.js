21091a23f68489730d314146dbee9c15dc23fe45
dimisavva <119276394+dimisavva@users.noreply.github.com>
2023-02-27 12:58:12 -0800

    update castRating functionality

--- controllers/ratings.js
+++ controllers/ratings.js
@@ -1,11 +1,29 @@
-const { Rating } = require('../models')
+const { Rating, Profile } = require('../models')
 
 async function castRating(req, res) {
   try {
     req.body.raterId = req.user.profile.id
 
-    const rating = await Rating.create(req.body)
-    res.status(200).json(rating)
+    const prevRating = await Rating.findOne({
+      where: {
+        raterId: req.body.raterId,
+        profileId: req.body.profileId
+      }
+    })
+  
+    if (prevRating) {
+      prevRating.value = req.body.value
+      await prevRating.save()
+    }else {
+      await Rating.create(req.body)
+    }
+
+    const profile = await Profile.findByPk(
+      req.body.profileId,
+      { include: [{ model: Rating, as: 'ratingsReceived' }]}
+    )
+
+    res.status(200).json(profile)
   } catch (error) {
     res.status(500).json({ err: error })
   }
